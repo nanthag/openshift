29fd4e8bfd9498770dec950490ae8dc52019726c
nanthags <nanthags@gmail.com>
2023-06-05 09:06:18 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace openshift.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
